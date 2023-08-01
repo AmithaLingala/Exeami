dc78a5c72b2f4b0a3a494214f6fd089725383dd8
amithalingala <amithalingala@tutamail.com>
2023-08-01 22:09:54 +0100

    fixed theme

--- html/scripts/util.js
+++ html/scripts/util.js
@@ -1,14 +1,15 @@
-function createElement(name, { attributes, classes } = {}) {
-  let element = document.createElement(name);
-  if (attributes) {
-    attributes.forEach((attribute) => {
-      element.setAttribute(attribute.name, attribute.value);
-    });
-  }
-  if (classes) {
-    element.classList.add(...classes);
-  }
-  return element;
+// function to set a given theme/color-scheme
+function selectTheme(themeName) {
+  localStorage.setItem('theme', themeName)
+  document.documentElement.className = themeName
 }
 
-export { createElement };
+window.onload = function () {
+  const themeButtons = document.querySelectorAll('.theme-button')
+  themeButtons.forEach((themeButton) => {
+    themeButton.onclick = () =>
+      selectTheme(themeButton.getAttribute('data-theme'))
+  })
+  const theme = localStorage.getItem('theme') || 'black-theme'
+  selectTheme(theme)
+}
