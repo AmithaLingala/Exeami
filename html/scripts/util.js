153426b7be93cf93443af97d91075346808a7a88
amithalingala <amithalingala@tutamail.com>
2023-08-03 22:06:56 +0100

    added theme switcher

--- html/scripts/util.js
+++ html/scripts/util.js
@@ -1,7 +1,16 @@
-function selectTheme(themeName) {
-  localStorage.setItem('theme', themeName)
-  document.documentElement.className = themeName
-  switchLogo(themeName)
+let themes = []
+
+function swapThemeButtons() {
+  const currentTheme = getTheme()
+  const themeSwitcher = document.querySelector('.theme-switcher-button')
+  themes.forEach((theme) => {
+    const themeButton = document.querySelector(`.theme-box .${theme}-button`)
+    if (theme === currentTheme) {
+      themeButton.classList.add('hide-in-mobile')
+    } else {
+      themeButton.classList.remove('hide-in-mobile')
+    }
+  })
 }
 
 function switchLogo(themeName) {
@@ -13,12 +22,60 @@ function switchLogo(themeName) {
   }
 }
 
+function selectTheme(themeName) {
+  const themeButton = document.querySelector(`.theme-box .${themeName}-button`)
+  const themeSwitcher = document.querySelector('.theme-switcher-button')
+
+  themeSwitcher.classList.replace(`${getTheme()}-button`, `${themeName}-button`)
+  themeSwitcher.textContent = themeButton.textContent
+
+  document.documentElement.className = themeName
+  switchLogo(themeName)
+  localStorage.setItem('theme', themeName)
+}
+
+function getTheme() {
+  return document.documentElement.className
+}
+
+function isDisplayed(element) {
+  return window.getComputedStyle(element).display !== 'none'
+}
+
+function getAllThemes() {
+  const themes = []
+  const themeBox = document.querySelector('.theme-box')
+  for (let child of themeBox.children) {
+    themes.push(child.classList[0].replace('-button', ''))
+  }
+  return themes
+}
+
 window.onload = function () {
+  themes = getAllThemes()
+
+  const themeSwitcher = document.querySelector('.theme-switcher-button')
+  const themeName = localStorage.getItem('theme') || themes[0]
+
+  themeSwitcher.classList.add(`${themeName}-button`)
+  selectTheme(themeName)
+
   const themeButtons = document.querySelectorAll('.theme-button')
+
   themeButtons.forEach((themeButton) => {
-    themeButton.onclick = () =>
+    themeButton.onclick = () => {
       selectTheme(themeButton.getAttribute('data-theme'))
+      swapThemeButtons()
+    }
   })
-  const theme = localStorage.getItem('theme') || 'black-theme'
-  selectTheme(theme)
+
+  themeSwitcher.onclick = () => {
+    swapThemeButtons()
+    const themeBox = document.querySelector('.theme-box')
+    if (themeBox.classList.contains('hide-in-mobile')) {
+      themeBox.classList.remove('hide-in-mobile')
+    } else {
+      themeBox.classList.add('hide-in-mobile')
+    }
+  }
 }
