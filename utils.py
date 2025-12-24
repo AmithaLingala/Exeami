da1f7e605c65494c327b461fb502cecd74627a80
AmithaLingala <amithalingala@tutamail.com>
2025-12-24 10:48:48 +0000

    Update blog list

--- utils.py
+++ utils.py
@@ -20,17 +20,17 @@ def get_output_path(path):
 
 
 def get_json_data(file_name):
-    with open(join("data", "{0}.json".format(file_name)), "r") as data_file:
+    with open(join("data", "{0}.json".format(file_name)), "r", encoding="utf8") as data_file:
         return json.loads(data_file.read())
 
 
 def read_file(file_name):
-    with open(file_name, "r") as data_file:
+    with open(file_name, "r", encoding="utf8") as data_file:
         return data_file.read()
 
 
 def write_file(file_name, content):
-    with open(file_name, "w") as data_file:
+    with open(file_name, "w", encoding="utf8") as data_file:
         data_file.writelines(content)
 
 _map = {
